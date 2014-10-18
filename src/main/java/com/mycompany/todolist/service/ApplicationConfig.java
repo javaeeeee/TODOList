0cc3eb5f5663d4c01df5643f60049059867524c3
javaeeeee <javaeeeee@gmail.com>
2014-10-18 12:39:32 +0400

    Added some CRUD methods

--- src/main/java/com/mycompany/todolist/service/ApplicationConfig.java
+++ src/main/java/com/mycompany/todolist/service/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.mycompany.todolist.NewCrossOriginResourceSharingFilter.class);
         resources.add(com.mycompany.todolist.service.TasksFacadeREST.class);
         resources.add(com.mycompany.todolist.service.UsersFacadeREST.class);
     }
--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,82 +1,113 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-angular.module('app', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('login', {
-        url: '/login',
-        templateUrl: 'partials/login.html',
-        controller: 'LoginCtrl'
-    }).state('main', {
-        url: '/main',
-        templateUrl: 'partials/main.html',
-        controller: 'MainCtrl'
-    }).state('add', {
-        url:'/add',
-        templateUrl:'partials/edit.html',
-        controller:'AddCtrl'
-    }).state('edit',{
-        url:'/edit',
-        templateUrl:'partials/edit.html',
-        controller:'EditCtrl'
-    }).state('history',{
-        url:'/history',
-        templateUrl:'partials/history.html',
-        controller:'HistoryCtrl'
-    });
-    $urlRouterProvider.otherwise('login');
-}).factory('users', function ($http) {
-    var users;
-    $http.get('/TODOList/api/user').success(function (data) {
-        //$scope.users = data;
-        users = data;
-    });
-    return users;
-}).factory('items', function () {
+//Add a module to the application
+angular.module('app', ['ui.router', 'ngResource'])
+        //Describe application states
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider.state('login', {
+                url: '/login',
+                templateUrl: 'partials/login.html',
+                controller: 'LoginCtrl'
+            }).state('main', {
+                url: '/main',
+                templateUrl: 'partials/main.html',
+                controller: 'MainCtrl'
+            }).state('add', {
+                url: '/add',
+                templateUrl: 'partials/edit.html',
+                controller: 'AddCtrl'
+            }).state('edit', {
+                url: '/edit{id}',
+                templateUrl: 'partials/edit.html',
+                controller: 'EditCtrl'
+            }).state('history', {
+                url: '/history',
+                templateUrl: 'partials/history.html',
+                controller: 'HistoryCtrl'
+            }).state('delete', {
+                url: '/main{id}',
+                templateUrl: 'partials/main.html',
+                controller: 'MainCtrl'
+            });
+            $urlRouterProvider.otherwise('login');
+        })
+        //A factory that produces a resource to work with TODO items  
+        .factory('Item', function ($resource) {
+            return $resource('api/tasks/:id', null, {
+                'update': {
+                    method: 'PUT'
+                }
+            });
+        })
+        //A factory with special methods for login and logout
+        .factory('service', function ($state, users) {
+            var logout = function () {
+                $state.go('login');
+            };
+            var getAuthenticatedUser = function (login, password) {
+                var users = users.users;
+            };
+            return {
+                logout: logout
+            };
+        })
+        // A factory to produce atatuses of TODO items        
+        .factory('statuses', function () {
+            return ['High', 'Medium', 'Low'];
+        })
+        //Controller of the main page
+        .controller('MainCtrl', function ($log, $scope, $state, service, Item) {
+            $scope.logout = service.logout;
+            Item.query(function (items) {
+                $scope.items = items;
+            });
+            $scope.addItem = function () {
+                $state.go('add');
+            };
+            $scope.showHistory = function () {
+                $state.go('history');
+            };
+            $scope.deleteItem = function (id) {
+                Item.delete({id: id});
+            };
+        })
+        //Controller of the login page
+        .controller('LoginCtrl', function ($log, $scope, $state, $resource, $http, users) {
+            //if($scope.login&&$scope.password)
+            //TODO A temporary stub
+            $scope.doLogin = function () {
+                $state.go('main');
+            };
+        })
+        //Controller to edit TODO items
+        .controller('EditCtrl', function ($log, $scope, $stateParams, $state, statuses, Item) {
+            $scope.action = "Edit Item";
+            $scope.statuses = statuses;
+            $scope.item = Item.get({id: $stateParams.id});
+            $scope.saveItem = function () {
+                var $id = $scope.item.id;
+                Item.update({id: $id}, $scope.item, function () {
+                    $state.go('main');
+                }, function (reason) {
+                    $log.error('An error occured saving an item ' + reason.status);
+                    $log.debug('data = ' + JSON.stringify(reason.config.data));//JSON.stringify(reason)
+                });
+            };
+        })
+        //Controller for a history page        
+        .controller('HistoryCtrl', function ($scope) {
 
-
-}).factory('service', function ($state, users) {
-    var logout = function () {
-        $state.go('login');
-    };
-    var getAuthenticatedUser = function (login, password) {
-        var users = users.users;
-    };
-    return {
-        logout: logout
-    };
-}).controller('MainCtrl', function ($log, $scope, $http, $state, service) {
-    $scope.logout = service.logout;
-    var get = $http.get('api/tasks');///TODOList
-    get.success(function (data) {
-        $scope.items = data;
-    });
-    get.error(function (data, status, headers, cinfig) {
-        $log.log('No data received');
-    });
-    $scope.addItem=function (){
-        $state.go('add');
-    };
-    $scope.showHistory=function (){
-        $state.go('history');
-    };
-}).controller('LoginCtrl', function ($scope, $state) {
-    //if($scope.login&&$scope.password)
-    $scope.doLogin = function () {
-        $state.go('main');
-    };
-}).controller('EditCtrl', function ($scope, statuses) {
-    $scope.action = "Edit Item";
-    $scope.statuses = statuses;
-}).controller('HistoryCtrl', function ($scope) {
-
-}).controller('AddCtrl', function ($scope, statuses) {
-    $scope.action = "Add Item";
-    $scope.statuses = statuses;
-}).controller('HistoryCtrl', function ($scope){
-
-}).factory('statuses',function (){
-    return ['High','Medium','Low'];
-});
+        })
+        // Controller for adding items
+        .controller('AddCtrl', function ($log, $scope, $state, statuses, Item, users) {
+            $scope.action = "Add Item";
+            $scope.statuses = statuses;
+            $scope.item = new Item();
+            $scope.item.name = "dgdgdg";
+            $scope.item.userId = $scope.currentUser;
+            $log.debug(JSON.stringify($scope.currentUser));
+            $scope.saveItem = function () {
+                Item.save($scope.item, function () {
+                    $state.go('main');
+                });
+            };
+        });
